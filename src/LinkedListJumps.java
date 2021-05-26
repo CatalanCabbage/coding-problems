1f39854dd4dd702b075e6362f1190eeb388250a8
CatalanCabbage <45961072+CatalanCabbage@users.noreply.github.com>
2021-05-26 14:55:19 +0530

    Create LinkedListJumps.java

---
+++ src/LinkedListJumps.java
@@ -0,0 +1,42 @@
+/**
+ * Problem: 
+ * You are given a singly linked list node containing positive integers. 
+ * Return the same linked list where every node's next points to the node val nodes ahead. 
+ * If there's no such node, next should point to null.
+ *
+ * Eg: Input: [2, 1, 4, 1], Output: [2, 4]
+ * Explanation: Note that 2's next is 2 node ahead. 4's next is out of bounds, so it's set to null.
+ *
+ * Link: https://binarysearch.com/problems/Linked-List-Jumps
+ */
+
+import java.util.*;
+
+/**
+ * class LLNode {
+ *   int val;
+ *   LLNode next;
+ * }
+ */
+
+class LinkedListJumps {
+    public LLNode solve(LLNode node) {
+        LLNode pointer1 = node;
+        LLNode pointer2 = node;
+
+        //Move pointer2 ahead `val` times
+        //Make pointer1 point to the `val`th node
+        //Repeat
+        while (pointer2 != null) {
+            int jumps = pointer2.val;
+            //Keep moving ahead till either `val`th node or a null node is reached
+            for (int i = 0; i < jumps && pointer2 != null; i++) {
+                pointer2 = pointer2.next;
+            }
+            //Update the value in the same List
+            pointer1.next = pointer2;
+            pointer1 = pointer1.next;
+        }
+        return node;
+    }
+}
