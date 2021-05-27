5fd504f6da00d1fbbe5f4180c462a608aa848445
CatalanCabbage <45961072+CatalanCabbage@users.noreply.github.com>
2021-05-27 14:09:54 +0530

    Create ReverseLinkedList2.java

---
+++ src/ReverseLinkedList2.java
@@ -0,0 +1,31 @@
+/**
+ * Problem: 
+ * Given the head of a singly linked list and two integers left and right where left <= right, 
+ * reverse the nodes of the list from position left to position right, and return the reversed list.
+ * 
+ * Link: https://leetcode.com/problems/reverse-linked-list-ii/
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class ReverseLinkedList2 {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+      if (head == null) {
+        return head;
+      }
+      int currentPosition = 0;
+      ListNode previous = 
+      while (head != null) {
+         
+      }
+    }
+}
