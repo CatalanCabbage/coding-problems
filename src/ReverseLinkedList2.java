e62d3c6f5f17d590113bbc0f2d3963937a110bcf
CatalanCabbage <45961072+CatalanCabbage@users.noreply.github.com>
2021-05-28 19:28:08 +0530

    Update ReverseLinkedList2.java

--- src/ReverseLinkedList2.java
+++ src/ReverseLinkedList2.java
@@ -17,15 +17,55 @@
  * }
  */
 
+/**
+ * Move normally till Left edge
+ * Start reversing
+ * Join the last to right edge
+ * Edge case: left == 1
+ */
 class ReverseLinkedList2 {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-      if (head == null) {
-        return head;
-      }
-      int currentPosition = 0;
-      ListNode previous = 
-      while (head != null) {
-         
-      }
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode headRef = head;
+        int currentNode = 1;
+        ListNode prev = null;
+        while (currentNode < left) {
+            prev = head;
+            head = head.next;
+            currentNode++;
+        }
+        //At this point, head == left
+        //prev is the left edge
+        ListNode leftEdge = prev;
+
+        //Move prev to the first node to be reversed
+        prev = head;
+        head = head.next;
+        currentNode++;
+
+        //prev is the first node now
+        ListNode rightRevEdge = prev;
+
+        //Reverse till right edge is reached. In the end, head is rightEdge.
+        while (currentNode <= right) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+            currentNode++;
+        }
+
+        //When `left == 1` - `left` is the first element, there is no unchanged left list.
+        //So `prev` is the head, similar to a normal reversed linked list
+        if(leftEdge != null) {
+            leftEdge.next = prev;
+        } else {
+            headRef = prev;
+        }
+        rightRevEdge.next = head;        
+        return headRef;
     }
 }
