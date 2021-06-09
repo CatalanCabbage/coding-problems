3839cc4d9bc707526de1946fbd4f2ed89608cb07
CatalanCabbage <45961072+CatalanCabbage@users.noreply.github.com>
2021-06-09 16:34:18 +0530

    Create FirstMissingPositive.java

---
+++ src/FirstMissingPositive.java
@@ -0,0 +1,29 @@
+/**
+ * Problem: 
+ * Given a list of integers nums, find the first missing positive integer. In other words, find the lowest positive integer that does not exist in the list. 
+ * The list can contain duplicates and negative numbers as well. 
+ * Link: https://binarysearch.com/problems/First-Missing-Positive
+ */
+
+import java.util.*;
+
+//Just sort and iterate, too easy.
+class Solution {
+    public int FirstMissingPositive(int[] nums) {
+        Arrays.sort(nums);
+        int currentNum = 1;
+        for (int i = 0; i < nums.length; i++) {
+            //Can do a binary search here to arrive at 1
+            if (nums[i] <= 0) {
+                continue;
+            }
+            //This means it's a repeated number
+            if (nums[i] < currentNum) {
+                continue;
+            } else if (nums[i] == currentNum) {
+                currentNum++;
+            }
+        }
+        return currentNum;
+    }
+}
