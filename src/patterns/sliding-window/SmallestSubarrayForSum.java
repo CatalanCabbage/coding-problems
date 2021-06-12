3bafd200e001eb1d5b4da767d1895024ea14e73b
CatalanCabbage <45961072+CatalanCabbage@users.noreply.github.com>
2021-06-12 12:47:06 +0530

    Create SmallestSubarrayForSum.java

---
+++ src/patterns/sliding-window/SmallestSubarrayForSum.java
@@ -0,0 +1,18 @@
+/**
+ * Given an array of positive numbers and a positive number S: 
+ * Find the length of the smallest contiguous subarray whose sum is greater than or equal to S. 
+ * Return 0 if no such subarray exists.
+ *
+ * Eg: Input: [2, 1, 5, 2, 3, 2], S=7 
+ * Output: 2 //[5, 2]
+ *
+ * Input: [2, 1, 5, 2, 8], S=7 
+ * Output: 1 //[8]
+ */
+
+class SmallestSubarrayForSum {
+  public static int findMinSubArray(int S, int[] arr) {
+    // TODO: Write your code here
+    return 0;
+  }
+}
