c44483bf2ee747b3ed73f77a848d2e3eb53bdf23
CatalanCabbage <45961072+CatalanCabbage@users.noreply.github.com>
2021-07-08 15:07:07 +0530

    3sum closest

--- src/patterns/two_pointer/ThreeSumClosest.java
+++ src/patterns/two_pointer/ThreeSumClosest.java
@@ -13,9 +13,50 @@ import java.util.List;
  * You may assume that each input would have exactly one solution.
  *
  * Link: https://leetcode.com/problems/3sum-closest/
+ *
+ * Take the most suitable metric to model the problem! Here, diff is our aim, so take diff instead of sum.
  */
 
 public class ThreeSumClosest {
+    int smallestDiff = Integer.MAX_VALUE; //Math.abs(Target - sum)
+    int target;
+    public int threeSumClosest(int[] nums, int target) {
+        this.target = target;
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            twoSum(i, nums);
+        }
+        return target - smallestDiff;
+    }
+
+    private void twoSum(int n, int[] nums) {
+        int p1 = n + 1;
+        int p2 = nums.length - 1;
+
+        while (p1 < p2) {
+            int diff = target - nums[n] - nums[p1] - nums[p2];
+            //Can short circuit the whole problem here
+            if (diff == 0) {
+                smallestDiff = diff;
+                return;
+            }
+
+            if (Math.abs(diff) < Math.abs(smallestDiff)) {
+                smallestDiff = diff;
+            }
+
+            //Aim is to reduce diff to 0; here, increase in p1 == increase in sum == decrease in diff
+            if (diff > 0) {
+                p1++;
+            } else {
+                p2--;
+            }
+        }
+    }
+
+    /*
     int solution;
     int target;
     public int threeSumClosest(int[] nums, int target) {
@@ -56,4 +97,5 @@ public class ThreeSumClosest {
             }
         }
     }
+    */
 }
