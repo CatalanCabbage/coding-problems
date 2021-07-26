4a82b1ba46d466ff623f311ac8031ac7e75290f8
CatalanCabbage <45961072+CatalanCabbage@users.noreply.github.com>
2021-07-26 18:30:43 +0530

    Fixed 4sum

--- src/patterns/two_pointer/FourSum.java
+++ src/patterns/two_pointer/FourSum.java
@@ -3,6 +3,10 @@
  */
 package patterns.two_pointer;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
  * 0 <= a, b, c, d < n
@@ -16,11 +20,12 @@ package patterns.two_pointer;
  */
 
 public class FourSum {
-  List<List<Integer>> soln = new LinkedList<>();
-    public List<List<Integer>> fourSum(int[] nums, int target) {   
+    static List<List<Integer>> soln = new LinkedList<>();
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
         for (int i = 0; i < nums.length - 3; i++) {
-            while (i > 0 && nums[i] == nums[i - 1]) {
+            //<bug>Had used `while` instead of `if` here, which means i++ never happened
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
             threeSum(i + 1, nums, target - nums[i]);
@@ -28,16 +33,17 @@ public class FourSum {
         return soln;
     }
 
-    private void threeSum(int index, int[] nums, int target) {
+    private static void threeSum(int index, int[] nums, int target) {
         for (int i = index; i < nums.length - 2; i++) {
             if (i > index && nums[i] == nums[i - 1]) {
                 continue;
             }
-            twoSum(i + 1, nums, target - nums[i], new int[]{nums[index], nums[i]});
+            //<bug> had used `nums[index]` here - had missed the `i + 1` done in the caller function
+            twoSum(i + 1, nums, target - nums[i], new int[]{nums[index - 1], nums[i]});
         }
     }
     
-    private void twoSum(int index, int[] nums, int target, int[] path) {
+    private static void twoSum(int index, int[] nums, int target, int[] path) {
         int left = index;
         int right = nums.length - 1;
         while (left < right) {
@@ -63,5 +69,10 @@ public class FourSum {
                 }
             }
         }
+    }
+
+    public static void main(String[] args) {
+        List soln = fourSum(new int[]{2,2,2,2,2}, 8);
+        System.out.println(soln);
     }
 }
