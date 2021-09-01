e66c869267718896ce3f664d0bc61141be521549
CatalanCabbage <45961072+CatalanCabbage@users.noreply.github.com>
2021-09-01 16:53:13 +0530

    Add disappearedNum

--- src/patterns/cyclic_sort/FindAllNumbersDisappearedInArray.java
+++ src/patterns/cyclic_sort/FindAllNumbersDisappearedInArray.java
@@ -3,39 +3,46 @@
  */
 package patterns.cyclic_sort;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Problem:
- * Given an array nums containing n distinct numbers in the range [0, n],
- * return the only number in the range that is missing from the array.
+ * Given an array nums of n integers where nums[i] is in the range [1, n],
+ * return an array of all the integers in the range [1, n] that do not appear in nums.
  *
- * Eg: Input: nums = [3,0,1]
- * Output: 2
- * Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2
- * is the missing number in the range since it does not appear in nums.
+ * Eg: Input: nums = [4,3,2,7,8,2,3,1]
+ * Output: [5,6]
  *
- * Link: https://leetcode.com/problems/missing-number/
+ * Link: https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
  */
 
 public class FindAllNumbersDisappearedInArray {
-    public int missingNumber(int[] nums) {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> missingNums = new ArrayList<>();
+
         int i = 0;
         while (i < nums.length) {
-            if (i != nums[i] && nums[i] != nums.length) {
-                swap(i, nums[i], nums);
-            } else {
+            if (nums[i] == i + 1) {
                 i++;
+            } else {
+                if (nums[nums[i] - 1] == nums[i]) {
+                    i++;
+                } else {
+                    swap(nums, i, nums[i] - 1);
+                }
             }
         }
-        int missingNumber = nums.length;
+
         for (i = 0; i < nums.length; i++) {
-            if (i != nums[i]) {
-                missingNumber = i;
+            if (nums[i] != i + 1) {
+                missingNums.add(i + 1);
             }
         }
-        return missingNumber;
+        return missingNums;
     }
 
-    private void swap(int i, int j, int[] nums) {
+    private void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
