c04f0e1b8eaf354b83540f4d0aa9473936b0850b
CatalanCabbage <45961072+CatalanCabbage@users.noreply.github.com>
2021-10-20 11:28:41 +0530

    Add permutations 2

--- src/patterns/subsets/Permutations2.java
+++ src/patterns/subsets/Permutations2.java
@@ -3,21 +3,25 @@
  */
 package patterns.subsets;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 /**
- * Given an array nums of distinct integers, return all the possible permutations.
- * You can return the answer in any order.
+ * GGiven a collection of numbers, nums, that might contain duplicates,
+ * return all possible unique permutations in any order.
  *
- * Eg. Input: nums = [1,2,3]
- * Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
  *
- * Link: https://leetcode.com/problems/permutations/
+ *
+ * Eg. Input: nums = [1,1,2]
+ * Output:
+ * [[1,1,2],
+ *  [1,2,1],
+ *  [2,1,1]]
+ * 
+ * Link: https://leetcode.com/problems/permutations-ii/
  */
 
-public class Permutations {
+public class Permutations2 {
     List<List<Integer>> solution = new LinkedList<>();
     int[] nums;
     public List<List<Integer>> permute(int[] nums) {
