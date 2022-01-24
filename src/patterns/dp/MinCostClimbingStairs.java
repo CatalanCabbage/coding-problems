0342512e1323d3f3a1e1bbb697f8e61146337cbb
CatalanCabbage <45961072+CatalanCabbage@users.noreply.github.com>
2022-01-24 16:26:44 +0530

    Add optimized versions of min cost

--- src/patterns/dp/MinCostClimbingStairs.java
+++ src/patterns/dp/MinCostClimbingStairs.java
@@ -18,11 +18,12 @@ package patterns.dp;
  */
 
 public class MinCostClimbingStairs {
-    public int minCostClimbingStairs(int[] costs) {
+    public int getMinCostRec(int[] costs) {
         int[] db = new int[costs.length + 1];
         return Math.min(getMinCost( 0, costs, db), getMinCost(1, costs, db));
     }
 
+    //Top-down
     private int getMinCost(int currentStep, int[] costs, int[] db) {
         if (currentStep >= costs.length) {
             return 0;
@@ -34,5 +35,45 @@ public class MinCostClimbingStairs {
             db[currentStep] = Math.min(costWithOneStep, costWithTwoSteps);
         }
         return db[currentStep];
+    }
+
+    private int getMinCostIter(int[] costs) {
+        int[] db = new int[costs.length];
+        for (int i = 0; i < db.length; i++) {
+            if (i < 2) {
+                db[i] = costs[i];
+            } else {
+                db[i] = costs[i] + Math.min(db[i - 1], db[i - 2]);
+            }
+        }
+        return Math.min(db[costs.length - 1], db[costs.length - 2]);
+    }
+
+    //Only the last 2 results are needed for us to calculate the next value.
+    //Ditch the array
+    private int getMinCostOptimizedIter(int[] costs) {
+        int first = 0;
+        int second = 1;
+        for (int i = 0; i < costs.length; i++) {
+            if (i < 2) {
+                int temp = costs[i];
+                second = first;
+                first = temp;
+            } else {
+                int temp = costs[i] + Math.min(first, second);
+                second = first;
+                first = temp;
+            }
+        }
+        return Math.min(first, second);
+    }
+
+    public static void main(String[] args) {
+        int[] inp = new int[]{1,100,1,1,1,100,1,1,100,1};
+        MinCostClimbingStairs minCostClimbingStairs = new MinCostClimbingStairs();
+        System.out.println(minCostClimbingStairs.getMinCostRec(inp));
+        System.out.println(minCostClimbingStairs.getMinCostIter(inp));
+        System.out.println(minCostClimbingStairs.getMinCostOptimizedIter(inp));
+
     }
 }
