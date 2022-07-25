99883bc53686ded31e23e993841fc5518ca8b1a6
CatalanCabbage <45961072+CatalanCabbage@users.noreply.github.com>
2022-07-26 00:37:51 +0530

    Add some stuff

--- src/TrappingRainWater.java
+++ src/TrappingRainWater.java
@@ -14,7 +14,42 @@ public class TrappingRainWater {
         System.out.println(trapped);
     }
 
-    public int trap(int[] heights) {
+    //Todo:
+    //https://leetcode.com/problems/trapping-rain-water/discuss/17414/A-stack-based-solution-for-reference-inspired-by-Histogram/400375
+
+    //https://leetcode.com/problems/trapping-rain-water/discuss/17395/A-different-O(n)-approach-easy-to-understand-and-simple-code
+    public int trap(int[] height) {
+        int length = height.length;
+        //Intuition:
+        //1. Each column contributes to the water count
+        //as long as there's a taller wall on the left and the right.
+        //2. How much does it contribute? (min height of enclosing walls - current height)
+
+        //Populate tallest walls to the left
+        int[] tallestWallLeft = new int[length];
+        tallestWallLeft[0] = height[0];
+        for (int i = 1; i < length; i++) {
+            tallestWallLeft[i] = Math.max(tallestWallLeft[i - 1], height[i]);
+        }
+
+        //Populate tallest walls to the right
+        int[] tallestWallRight = new int[length];
+        tallestWallRight[length - 1] = height[length - 1];
+        for (int i = length - 2; i >= 0; i--) {
+            tallestWallRight[i] = Math.max(tallestWallRight[i + 1], height[i]);
+        }
+
+        //Calculate contribution by each column
+        int totalWater = 0;
+        for (int i = 0; i < length; i++) {
+            if (Math.min(tallestWallLeft[i], tallestWallRight[i]) > height[i]) {
+                totalWater += Math.min(tallestWallLeft[i], tallestWallRight[i]) - height[i];
+            }
+        }
+        return totalWater;
+    }
+
+    public int trapBruteForce(int[] heights) {
         int leftEdge = 0;
         int trappedWater = 0;
         while (leftEdge < heights.length) {
